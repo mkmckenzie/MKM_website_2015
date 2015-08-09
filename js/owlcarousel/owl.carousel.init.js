6e16b955e3858f2bd9cbd5d46acee62b631443b0
mkmckenzie <mkmckenzie91@gmail.com>
2015-08-09 15:32:48 -0400

    Fix navigation bar, add animated elements, add jquery slider and lightbox

---
+++ js/owlcarousel/owl.carousel.init.js
@@ -0,0 +1,12 @@
+// Owl Carousel initialize javascript file or whatev
+
+$(document).ready(function(){
+        $(".owl-carousel").owlCarousel( {
+          items:3,
+          loop:true,
+          margin:10,
+          autoplay:true,
+          autoplayTimeout:2000,
+          autoplayHoverPause:true
+        });
+      });
